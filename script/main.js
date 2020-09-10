5e75d529e457e975196299bef0e0f1277e2346c7
Draknas <karesz0328@gmail.com>
2020-09-10 16:44:26 +0200

    design

---
+++ script/main.js
@@ -0,0 +1,24 @@
+    var url = 'https://openexchangerates.org/api/currencies.json';
+    fetch(url).then(function(response) {
+        response.text().then(function(text) {
+
+            var json = JSON.parse(text);
+
+            var from = document.getElementById('selectFrom');
+            var to = document.getElementById('selectTo');
+
+            for(var key in json) {
+                //console.log(key);
+                addOption(from, key, json[key]);
+                addOption(to, key, json[key]);
+
+            }
+        });
+    });
+
+    function addOption(parent, index, item) {
+        let element = document.createElement('option');
+        element.value = index;
+        element.innerHTML = index +' - '+ item;
+        parent.appendChild(element);
+    }
---
+++ script/test.php
@@ -0,0 +1,8 @@
+<?php
+    class sakk {
+    static function __construct() {
+        echo 'asd';
+    }
+}
+
+?>
