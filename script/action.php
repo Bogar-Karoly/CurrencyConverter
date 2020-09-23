e2ad75813a004ed2310ef5c86949a581a8c3d2cd
Draknas <karesz0328@gmail.com>
2020-09-23 16:18:02 +0200

    design, comment

--- script/action.php
+++ script/action.php
@@ -1,24 +1,4 @@
 <?php
-/*
-if(!isset($_POST['submit'])) {
-    $value = floatval(trim($_POST["Value"]));
-    $from = $_POST['From'];
-    $to = $_POST['To'];
-    $fromto = $from.'_'.$to;
-    $fromto = "EUR_HUF";
-
-    //$apiUrl = "https://free.currconv.com/api/v7/convert?q=".$fromto."&compact=ultra&apiKey=ff150b18677e80c649f4";
-    $apiUrl = "https://free.currconv.com/api/v7/convert?q=USD_PHP&compact=ultra&apiKey=ff150b18677e80c649f4";
-    $json = file_get_contents("{$apiUrl}");
-    $exchangeRate = json_decode($json,true);
-
-    $exchange = $value * floatval($exchangeRate["$fromto"]);
-    echo json_encode(number_format($exchange, 2, '.', ''));
-    //exit();
-}
-
-//echo json_encode('text');
-*/
 
 $value = floatval(trim($_POST['Value']));
 $from = $_POST['From'];
@@ -31,17 +11,18 @@ $newCurrency = $toEuro * floatval($data["{$to}"]);
 
 echo json_encode(number_format($newCurrency, 6, '.', ''));
 
-
 function Callapi() {
-    $ch = curl_init("http://data.fixer.io/api/latest?access_key=3e6e9740c403926dd5b3b09d26f47d59");
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
+    require_once('Url.php');
+
+    $curl = curl_init(GetUrl());
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    $result = curl_exec($curl);
+    curl_close($curl);
+
     $raw = json_decode($result,true);
     $data = $raw['rates'];
 
-    //print_r($data);
     return $data;
 }
 ?>
--- script/test.php
+++
@@ -1,8 +0,0 @@
-<?php
-    class sakk {
-    static function __construct() {
-        echo 'asd';
-    }
-}
-
-?>
